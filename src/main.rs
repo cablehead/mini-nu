855b7fd991da7499cc154aa052e6353dfdf028dd
Andy Gayton <andy@thecablelounge.com>
2024-08-08 11:24:11 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,27 @@ use nu_engine::eval_block_with_early_return;
 use nu_protocol::debugger::WithoutDebug;
 use nu_protocol::engine::{Stack, StateWorkingSet};
 use nu_protocol::{PipelineData, Span, Value};
-use std::sync::Arc;
 use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 1. Initialize base EngineState
     let mut base_engine_state = create_default_context();
     base_engine_state = add_shell_command_context(base_engine_state);
     base_engine_state = add_cli_context(base_engine_state);
 
-    // Gather parent environment variables
     let init_cwd = std::env::current_dir()?;
     gather_parent_env_vars(&mut base_engine_state, init_cwd.as_ref());
 
-    // 2. Get the code snippet
     let code_snippet = std::env::args().nth(1).expect("No code snippet provided");
 
-    // 3. Prepare the base engine state for cloning
-    let base_engine_state = Arc::new(base_engine_state);
+    // let base_engine_state = Arc::new(base_engine_state);
 
-    // 4. Create two threads, each running the snippet
     let threads: Vec<_> = (0..2)
         .map(|i| {
-            let engine_state = Arc::clone(&base_engine_state);
+            let engine_state = base_engine_state.clone();
             let snippet = code_snippet.clone();
 
             thread::spawn(move || {
-                let mut engine_state = (*engine_state).clone(); // Clone the EngineState
+                let mut engine_state = engine_state.clone();
                 let mut working_set = StateWorkingSet::new(&engine_state);
                 let block = nu_parser::parse(&mut working_set, None, snippet.as_bytes(), false);
 
@@ -69,7 +63,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .collect();
 
-    // 5. Wait for both threads to complete
     for thread in threads {
         thread.join().unwrap();
     }
