112d06688d7f7eaffbd0752f3c1342e6d8b5da84
Andy Gayton <andy@thecablelounge.com>
2024-08-09 17:38:29 -0400

    pass an argument to the supplied closure

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,7 @@ struct ThreadPool {
 
 impl ThreadPool {
     fn new(size: usize) -> Self {
-        let (tx, rx) =
-            crossbeam_channel::bounded::<Box<dyn FnOnce() + Send + 'static>>(0);
+        let (tx, rx) = crossbeam_channel::bounded::<Box<dyn FnOnce() + Send + 'static>>(0);
         let active_count = Arc::new(AtomicUsize::new(0));
         let completion_pair = Arc::new((Mutex::new(()), Condvar::new()));
 
@@ -125,27 +124,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let engine_state = engine_state.clone();
                 let closure = closure.clone();
                 let pool = pool.clone();
+                let job_number = i;
                 pool.execute(move || {
                     let mut stack = Stack::new();
-                    println!("Thread {} starting execution", i);
+                    println!("Thread {} starting execution", job_number);
                     let input = PipelineData::Value(Value::string(line, Span::unknown()), None);
-                    match eval_closure(&engine_state, &mut stack, &closure, input) {
+                    match eval_closure(&engine_state, &mut stack, &closure, input, job_number) {
                         Ok(pipeline_data) => match pipeline_data.into_value(Span::unknown()) {
                             Ok(value) => match value {
-                                Value::String { val, .. } => println!("Thread {}: {}", i, val),
+                                Value::String { val, .. } => {
+                                    println!("Thread {}: {}", job_number, val)
+                                }
                                 Value::List { vals, .. } => {
                                     for val in vals {
-                                        println!("Thread {}: {:?}", i, val);
+                                        println!("Thread {}: {:?}", job_number, val);
                                     }
                                 }
-                                other => println!("Thread {}: {:?}", i, other),
+                                other => println!("Thread {}: {:?}", job_number, other),
                             },
                             Err(err) => {
-                                eprintln!("Thread {}: Error converting pipeline data: {:?}", i, err)
+                                eprintln!(
+                                    "Thread {}: Error converting pipeline data: {:?}",
+                                    job_number, err
+                                )
                             }
                         },
                         Err(error) => {
-                            eprintln!("Thread {}: Error: {:?}", i, error);
+                            eprintln!("Thread {}: Error: {:?}", job_number, error);
                         }
                     }
                 });
@@ -174,19 +179,27 @@ fn eval_closure(
     stack: &mut Stack,
     closure: &Closure,
     input: PipelineData,
+    job_number: usize,
 ) -> Result<PipelineData, ShellError> {
     let block = &engine_state.get_block(closure.block_id);
-    for (_, var_id) in block.signature.required_positional.iter().enumerate() {
-        if let Some(var_id) = var_id.var_id {
-            stack.add_var(var_id, Value::string("", Span::unknown()));
-        } else {
-            return Err(ShellError::NushellFailedSpanned {
-                msg: "Error while evaluating closure".into(),
-                label: "closure argument missing var_id".into(),
-                span: Span::unknown(),
-            });
-        }
+
+    // Check if the closure has exactly one required positional argument
+    if block.signature.required_positional.len() != 1 {
+        return Err(ShellError::NushellFailedSpanned {
+            msg: "Closure must accept exactly one argument".into(),
+            label: format!(
+                "Found {} arguments, expected 1",
+                block.signature.required_positional.len()
+            )
+            .into(),
+            span: Span::unknown(),
+        });
     }
+
+    // Add job_number as the single argument
+    let var_id = block.signature.required_positional[0].var_id.unwrap();
+    stack.add_var(var_id, Value::int(job_number as i64, Span::unknown()));
+
     let eval_block_with_early_return = get_eval_block_with_early_return(engine_state);
     eval_block_with_early_return(engine_state, stack, block, input)
 }
